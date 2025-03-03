cd3b5d11e0a48dd0f053839d9b39cdcaa9c7c8f9
soc-adycoco <adycoco@storans.school.nz>
2025-03-03 14:08:56 +1300

    Create intro_3_1_games.py

---
+++ intro_3_1_games.py
@@ -0,0 +1,17 @@
+'''
+Intro to Python - Chapter 3.1
+by Adrienne Dycoco
+Date: 3/03/2025
+Version 1
+'''
+
+answer = int(input("How many hours a day do you play computer games? "))
+
+if answer < 2:
+  print("That seems a fairly healthy balance. Well done!")
+  
+elif answer < 4:
+  print("You’re probably good enough by now with all that practice.")
+else:
+  print("Put the controller down and get some fresh air once in a while!")
+  
