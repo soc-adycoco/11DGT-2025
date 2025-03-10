c9066a02471a429662a78227decf9a0c5ce6e6f5
soc-adycoco <adycoco@storans.school.nz>
2025-03-10 19:09:23 +1300

    Create while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,32 @@
+'''
+While Loops
+by Adrienne Dycoco
+Date: 4/03/2025
+Version 1
+'''
+
+#Loop counting from 10-1
+i = 10
+while i > 0:
+  print(i)
+  #Decrease i by 1
+  #i = i - i
+  i -= 1
+
+#Loop counting from 1-10
+i = 1
+while i <= 10:
+  print(i)
+  #Increase i by 1
+  #i = i + i
+  i += 1
+
+#
+names = ["Elon", "Trump", "RFK", "JD"]
+i = 0
+while i < len(names
+  print(names[1])
+  #Increase i by 1
+  #i = i - 1
+  i += 1
+print("Blast off!")
