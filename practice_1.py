98ed19f9f6c1635080366dced7d09285df96896b
soc-adycoco <adycoco@storans.school.nz>
2025-03-25 11:48:03 +1300

    Create practice_1.py

---
+++ practice_1.py
@@ -0,0 +1,37 @@
+"""
+Python Quiz Practice 1
+by Adrienne Dycoco
+Date: 20/03/2025
+Version 1
+"""
+
+grade = int(input("Please enter the score you got on a test: "))
+print("You entered: ", grade)
+
+#If grade is greater than or equal to 90:
+#   Print “A”
+if grade >= 90:
+  print("A")
+
+#Else if grade is greater than or equal to 80:
+#   Print “B”
+elif grade >= 80:
+  print("B")
+
+#Else if grade is greater than or equal to 70:
+#   Print “C”
+elif grade >= 70:
+  print("C")
+
+#Else if grade is greater than or equal to 60:
+#   Print “D”
+elif grade >= 60:
+  print("D")
+
+#Else if grade is greater than or equal to 50:
+#   Print “F”
+elif grade >= 50:
+  print("F")
+
+else:
+  print("nter a valid number!")
