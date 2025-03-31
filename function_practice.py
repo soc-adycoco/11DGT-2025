e77267d479043eb8ac6ff5cf2c2a47ec5dd9b8b5
soc-adycoco <adycoco@storans.school.nz>
2025-04-01 12:18:40 +1300

    Create function_practice.py

---
+++ function_practice.py
@@ -0,0 +1,16 @@
+"""
+Practice - Functions
+by Adrienne Dycoco
+Date: 25/03/2025
+Version 1
+"""
+
+def number_machine(x):
+  x = x * 10 / 2
+  print(x)
+  
+
+def multiply():
+  a = 2
+  b = 3
+  return a * b
