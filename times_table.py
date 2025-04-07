4b199f3bf5f95f8aa42a4eeb21dd1efb17509b9f
soc-adycoco <adycoco@storans.school.nz>
2025-04-07 14:15:31 +1200

    Update times_table.py
    Create function to check user input length
    If less than 1 or more than 25, display error message

--- times_table.py
+++ times_table.py
@@ -1,9 +1,28 @@
 """
-Times Table
+Times Table (and check input string length)
 by Adrienne Dycoco
 Date: 3/04/2025
 Version 1
 """
+
+#Example: define function to check input string length
+def check_length(str, msg):
+  while True:
+    if len(str) < 1 or len(str) > 25:
+      print("Error - length needs to be 1-25 characters. Try again!")
+      str = input(msg)
+    else:
+      print("You entered: ", str)
+      return str
+      break
+    
+msg = "What's your name?"
+#Get user input
+user_name = input(msg)
+#Call function to validate string
+user_name = check_length(user_name, msg)
+
+
 
 #################
 #Setup functions
